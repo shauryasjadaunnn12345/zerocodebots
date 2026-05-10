a31520c22a1189288352f7c40e7f019cd0bd5471
shaurya singh jadaun <shauryasjadaunnn@gmail.com>
2026-05-10 07:32:40 +0530

    Refactor models.py to improve structure and imports

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 import secrets
-
+import string
 
 def generate_bot_key():
     return ''.join(secrets.choice(string.ascii_letters + string.digits) for _ in range(32))
